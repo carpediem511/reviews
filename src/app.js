ef296193674e71e5a672a6329d32d6e1229f9888
carpediem511 <vampire.0514@gmail.com>
2022-10-28 17:10:07 +0300

    исправила css

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ let reviews = [
     title: "Лендинг для школы английского языка",
     stack: "Адаптивная вёрстка с использованием html5, css3",
     text: "У тебя всё очень хорошо получается, видно прогресс. Молодец, что дополнительно разобралась с формой - это очень полезный и важный навык уметь самостоятельно находить информацию. Отличное форматирование кода и хорошая вёрстка! Очень здорово, что используешь семантические теги, огромный плюс!",
-    author: "Сапожникова Лилия Евгеньевна, основатель онлайн-школы программирования BitByBit",
+    author:
+      "Сапожникова Лилия Евгеньевна, основатель онлайн-школы программирования BitByBit",
     date: "февраль, 2022 год",
   },
 
@@ -11,7 +12,8 @@ let reviews = [
     title: "Вёрстка формы",
     stack: "Адаптивная вёрстка с использованием html5, css3 и BEM",
     text: "Посмотрела форму. В целом мне нравится, всё логично и понятно. Наверное, хотелось бы чуть понятнее названия классов, но понимаю, что это сложно подбирать. Нравится, что используешь переменные и псевдоклассы, получились кастомные чекбоксы. И, главное, форма работает и корректно отправляются значения!",
-    author: "Сапожникова Лилия Евгеньевна, основатель онлайн-школы программирования BitByBit",
+    author:
+      "Сапожникова Лилия Евгеньевна, основатель онлайн-школы программирования BitByBit",
     date: "март, 2022 год",
   },
 
@@ -19,7 +21,8 @@ let reviews = [
     title: "Сайт для стоматологической клиники",
     stack: "Адаптивная вёрстка с использованием html, css и BEM",
     text: "Понравилась структура вёрстки и форматирование. Отлично получились градиенты и тени. Всё отлично выровнено по сетке - тут вообще нет замечаний по выравниваю элементов, идеально! Отдельный плюс за использование git, вижу много коммитов. И огромный плюс, конечно, за кастомный домен. Очень аккуратно и хорошо вышла адаптивная вёрстка, учитывая ещё то, что её надо было придумать самостоятельно. Сделать отдельную папку для стилей и изображений - тоже правильное решение. Работа одна из лучших, очень здорово получилось! Молодец!",
-    author: "Сапожникова Лилия Евгеньевна, основатель онлайн-школы программирования BitByBit",
+    author:
+      "Сапожникова Лилия Евгеньевна, основатель онлайн-школы программирования BitByBit",
     date: "апрель, 2022 год",
   },
 
@@ -27,7 +30,8 @@ let reviews = [
     title: "Сайт для школы танцев",
     stack: "Адаптивная вёрстка с использованием html, css и BEM",
     text: "Всё получилось настолько идеально, что у меня даже почти нет замечаний! И на любых экранах тоже всё отлично работает. Получилась лучшая работа, я очень довольна результатом!! Вижу и коммиты, и свой домен, и на сайте всё аккуратно оформлено по макету. Мобильная версия тоже отличная! Можно перетягивать размер экрана в Chrome и наслаждаться!",
-    author: "Сапожникова Лилия Евгеньевна, основатель онлайн-школы программирования BitByBit",
+    author:
+      "Сапожникова Лилия Евгеньевна, основатель онлайн-школы программирования BitByBit",
     date: "май, 2022 год",
   },
 ]
@@ -41,7 +45,6 @@ let buttonRight = document.getElementById("button-right")
 let buttonLeft = document.getElementById("button-left")
 
 buttonRight.addEventListener("click", () => {
-  
   let review = reviews[0]
 
   let currentReview = reviews[currentIndex]
@@ -51,23 +54,32 @@ buttonRight.addEventListener("click", () => {
   content.innerHTML = ` 
   
     <div class="review">
-  
-                      <div class="review-image"><img class="review-person" src="/images/photo-person.png"/></div>
+
+    <div class="review-image"><img class="review-person" src="/images/photo-person.png"/></div>
+
+    <div class="review-list">
+
+      <div> <img class="review-point" src="/images/points.png"/> </div>
+      <div class="review-title">${review.title}</div>
+      <div class="review-stack">${review.stack}</div>
+      <div class="review-text">${review.text}</div>
+      <div class="review-author">${review.author}</div> 
+
+      <div class="reviewDateArray">
+        <div class="review-date">${review.date}</div>
+
+        
+      </div>
+
       
-                      <div class="review-list">
+    </div>
+
+    <div class="buttons">
+        <button id="button-left"> <img class="review-button" src="/images/icon-left.png" /> </button>
+    <button id="button-right"> <img class="review-button" src="/images/icon-right.png" /> </button>
+</div>
+</div> 
   
-                        <div> <img class="review-point" src="/images/points.png"/> </div>
-                        <div class="review-title">${review.title}</div>
-                        <div class="review-stack">${review.stack}</div>
-                        <div class="review-text">${review.text}</div>
-                        <div class="review-author">${review.author}</div>
-      
-                        <div class="reviewDateArray">
-                          <div class="review-date">${review.date}</div>
-                          <button id="button-right"> <img class="review-button" src="/images/icon-right.png" /> </button>
-                          <button id="button-left"> <img class="review-button" src="/images/icon-left.png" /> </button>
-                        </div>
-                      </div>
-                  </div>
+                
   `
 })
